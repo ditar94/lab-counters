57ca501531352bd9eb27431b16fa1d329a87eee5
Dionicy <dionicy@bu.edu>
2024-06-01 16:09:22 -0400

    updated fetal counter to use number keys.

--- fetal.js
+++ fetal.js
@@ -1,22 +1,24 @@
 
-window.field_1 = localStorage.getItem('field_1') || 0;
-window.field_2 = localStorage.getItem('field_2') || 0;
-window.field_3 = localStorage.getItem('field_3') || 0;
-window.field_4 = localStorage.getItem('field_4') || 0;
-window.field_5 = localStorage.getItem('field_5') || 0;
-window.average_RBC = localStorage.getItem('average_RBC') || 0;
-window.thirty_Fields = localStorage.getItem('thirty_Fields') || 0;
-window.field_7 = localStorage.getItem('field_7') || 0;
-window.field_6 = localStorage.getItem('field_6') || 0;
-window.add_Subtract1 = localStorage.getItem('add_Subtract1') || "Add";
-window.add_Subtract2 = localStorage.getItem('add_Subtract2') || "Add";
-window.add_Subtract3 = localStorage.getItem('add_Subtract3') || "Add";
-window.add_Subtract4 = localStorage.getItem('add_Subtract4') || "Add";
-window.add_Subtract5 = localStorage.getItem('add_Subtract5') || "Add";
-window.add_Subtract6 = localStorage.getItem('add_Subtract6') || "Add";
-window.percent_Fetals = localStorage.getItem('percent_Fetals') || 0;
-document.addEventListener("input", updateValue);
+let field_1 = 0;
+let field_2 = 0;
+let field_3 = 0;
+let field_4 = 0;
+let field_5 = 0;
+let average_RBC = 0;
+let thirty_Fields = 0;
+let field_7 = 0;
+let field_6 = 0;
+let add_Subtract1 = "Add";
+let add_Subtract2 = "Add";
+let add_Subtract3 = "Add";
+let add_Subtract4 = "Add";
+let add_Subtract5 = "Add";
+let add_Subtract6 = "Add";
+let percent_Fetals = 0;
+let testNum = 0;
 document.addEventListener("click", inputSwitcher);
+document.addEventListener("input", updateValue);
+
 
 function inputSwitcher() {
     var hideMe = document.querySelectorAll('.hide-counters');
@@ -38,9 +40,8 @@ function inputSwitcher() {
         document.querySelectorAll('.alt_count')[1].style.visibility = "visible";
         document.querySelectorAll('.alt_count')[0].style.maxHeight ="none";
         document.querySelectorAll('.alt_count')[1].style.maxHeight = "none";
-
-
     }
+
 }
 
 function updateValue() {
@@ -49,8 +50,6 @@ function updateValue() {
     field_3 = document.getElementById('red-count-input').value;
     field_4 = document.getElementById('red-count-input').value;
     field_5 = document.getElementById('red-count-input').value;
-    console.log(average_RBC);
-    console.log(document.getElementById('red-count-input'))
     updateCounter();
     calculateAverage();
     calculatePercentage();
@@ -108,77 +107,88 @@ function resetCounter(thisCounter) {
     calculatePercentage();
 }
 
-function counterSave(fieldValue) {
-    localStorage.setItem(fieldValue, window[fieldValue]);
-    document.getElementById(fieldValue.replace("_", "")).textContent = window[fieldValue];
-    if (document.getElementById('noCounter').checked) {
-        field_2 = field_1;
-        field_3 = field_1;
-        field_4 = field_1;
-        field_5 = field_1;
-    }
-}
+// function counterSave(fieldValue) {
+//     document.getElementById(fieldValue.replace("_", "")).textContent = eval(fieldValue);
+//     if (document.getElementById('noCounter').checked) {
+//         field_2 = field_1;
+//         field_3 = field_1;
+//         field_4 = field_1;
+//         field_5 = field_1;
+//     }
+// }
 
 function buttonChange(thisButton) {
-    const buttonVariableMap = {
-        "s1": "add_Subtract1",
-        "s2": "add_Subtract2",
-        "s3": "add_Subtract3",
-        "s4": "add_Subtract4",
-        "s5": "add_Subtract5",
-        "s6": "add_Subtract6"
-    };
-    const currentButton = buttonVariableMap[thisButton];
-    if (currentButton && window[currentButton]) {
-        window[currentButton] = (window[currentButton] == "Add") ? "Subtract" : "Add";
-        counterSave(currentButton);
-    };
+    // const buttonVariableMap = {
+    //     "s1": "add_Subtract1",
+    //     "s2": "add_Subtract2",
+    //     "s3": "add_Subtract3",
+    //     "s4": "add_Subtract4",
+    //     "s5": "add_Subtract5",
+    //     "s6": "add_Subtract6"
+    // };
+    // const currentButton = buttonVariableMap[thisButton];
+    // if (currentButton && eval(currentButton) {
+    //     eval(currentButton) = (eval(currentButton) == "Add") ? "Subtract" : "Add";
+    //     counterSave(currentButton);
+    // };
+    console.log("thisButton is " + thisButton);
+    console.log("eval(thisButton) is " + eval(thisButton));
+
+    (eval(thisButton) == 'Add') ? eval(thisButton + "= 'Subtract'"): eval(thisButton + "= 'Add'");
+    updateCounter();
+
+
+    // currentButton = (eval(eval(currentButton)) == "Add") ? "Subtract" : "Add";
+    // updateCounter();
+
 }
 
 function calculateAverage() {
     average_RBC = (parseInt(field_1) + parseInt(field_2) + parseInt(field_3) + parseInt(field_4) + parseInt(field_5)) / 5;
     thirty_Fields = parseInt(average_RBC * 30);
-    counterSave("average_RBC");
-    counterSave("thirty_Fields");
+    updateCounter()
 }
 
 function keyStroke(event, inputId) {
     let buttonCode = 'add_Subtract' + String(inputId);
-    let element = 'field' + String(inputId);
-    console.log("The event is: " + event);
-    let valueCode = element.replace('d', 'd_');
-    let testNum = -1;
+    console.log("The event.key is: " + event.key);
+    let valueCode = 'field_' + String(inputId);
+    console.log("TestNum is: " + String(testNum));
+    console.log("valueCode is: " + String(valueCode));
+    console.log("eval(valueCode) is: " + String(eval(valueCode)));
+    console.log("eval(buttonCode) is: " + String(eval(buttonCode)));
+
+
+
+
         switch (inputId) {
             case 1:
             case 2:
             case 3:
             case 4:
             case 5:
-                console.log("The key is: " + event.key);
-                if (event.key === "ArrowRight" &&  window[buttonCode] == "Add") {
-                    window[valueCode]++;
+                if (event.key == "3" && eval(buttonCode) == "Add") {
+                    eval(valueCode + '++');
                     testNum++;
-                } else if (event.key == "ArrowRight" && window[buttonCode] == "Subtract" && window[valueCode] > 0) {
-                    window[valueCode]--;
+                    console.log("TestNum is: " + String(testNum));
+                } else if (event.key == "3" && eval(buttonCode) == "Subtract" && eval(valueCode)> 0) {
+                    eval(valueCode + '--');
                 }
-                counterSave(valueCode);
-                console.log("testNum is: " + testNum + ". buttonCode is: " + buttonCode + ". element is: " + element + ". valueCode is: " + valueCode + " which is equal to: " + window[valueCode]);
                 break;
             case 6:
-                if (event.key == "ArrowRight" &&  window[buttonCode] == "Add" && field_7 < 30) {
+                if (event.key == "3" &&  eval(buttonCode) == "Add" && field_7 < 30) {
                     field_7++;
-                } else if (event.key == "ArrowRight" && window[buttonCode] == "Subtract" && field_7 > 0) {
+                } else if (event.key == "3" && eval(buttonCode) == "Subtract" && field_7 > 0) {
                     field_7--;
-                } else if (event.key == "ArrowLeft" && window[buttonCode] == "Add" && field_7 < 30) {
+                } else if (event.key == "1" && eval(buttonCode) == "Add" && field_7 < 30) {
                     field_6++;
-                } else if (event.key == "ArrowLeft" && window[buttonCode] == "Subtract" && field_6 > 0) {
+                } else if (event.key == "1" && eval(buttonCode) == "Subtract" && field_6 > 0) {
                     field_6--;
                 }
-                counterSave("field_6");
-                counterSave("field_7");
                 break;
 
         }
+    updateCounter();
     calculateAverage();
     calculatePercentage();
 
@@ -187,36 +197,36 @@ function keyStroke(event, inputId) {
 function buttonPress(inputId) {
     console.log("inputId is " + inputId);
     let buttonCode = 'add_Subtract' + String(inputId);
-    let element = 'field' + String(inputId);
-    let valueCode = element.replace('d', 'd_');
+    let valueCode = 'field_' + String(inputId);
     if (inputId == "7") {
         buttonCode = 'add_Subtract' + String(parseInt(inputId)-1);
     }
+    console.log("TestNum is: " + String(testNum));
     switch (inputId) {
         case 1:
         case 2:
         case 3:
         case 4:
         case 5:
-            if (window[buttonCode] == "Add") {
-                window[valueCode]++;
-            } else if (window[buttonCode] == "Subtract" && window[valueCode] > 0) {
-                window[valueCode]--;
+            if (eval(buttonCode) == "Add") {
+                eval(valueCode + '++');
+                testNum++;
+                console.log("TestNum is: " + String(testNum));
+
+            } else if (eval(buttonCode) == "Subtract" && eval(valueCode) > 0) {
+                eval(valueCode + '--');
             }
             break;
         case 6:
         case 7:
-            if (window[buttonCode] == "Add" && field_7 < 30) {
-                window[valueCode]++;
-            } else if (window[buttonCode] == "Subtract" && window[valueCode] > 0) {
-                window[valueCode]--;
+            if (eval(buttonCode) == "Add" && field_7 < 30) {
+                eval(valueCode + '++');
+            } else if (eval(buttonCode) == "Subtract" && eval(valueCode) > 0) {
+                eval(valueCode + '--');
             }
             break;
     }
-
-    console.log("buttonCode is " + buttonCode);
-    console.log("valueCode is " + valueCode);
-    counterSave(valueCode);
+    updateCounter();
     calculateAverage();
     calculatePercentage();
 }
@@ -226,5 +236,5 @@ function buttonPress(inputId) {
 
 function calculatePercentage() {
     percent_Fetals = ((field_6 / thirty_Fields ) * 100).toFixed(1);
-    counterSave("percent_Fetals");
+    updateCounter();
 }
