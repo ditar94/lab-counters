959bf0073f40fafcf6eb765361ac4ad0850cb16d
Dionicy <dionicy@bu.edu>
2024-06-01 13:25:40 -0400

    Changed input from arrow keys to 1 and 3 for parasite counter. Also changed field to reaonly

--- parasite.js
+++ parasite.js
@@ -1,19 +1,12 @@
-let parasiteCount = localStorage.getItem('parasiteCount') || 0;
-let totalCountP = localStorage.getItem('totalCountP') || 0;
-let percentageP = localStorage.getItem('percentageP') || 0;
-let buttonStateP = localStorage.getItem('buttonStateP') || "Add";
+let parasiteCount = 0;
+let totalCountP = 0;
+let percentageP = 0;
+let buttonStateP = "Add";
 const input = document.querySelector("#count-input");
 const radio = document.querySelector("#wantCounter");
 const log = document.getElementById('para');
-document.addEventListener("input", updateValue);
+//document.addEventListener("input", updateValue);
 document.addEventListener("click", inputSwitcher);
-
-
-
-// function updateValue() {
-//     log.textContent = e.target.value;
-// }
-
 
 
 function inputSwitcher() {
@@ -43,14 +36,6 @@ function inputSwitcher() {
     }
 }
 
-function updateValue() {
-    parasiteCount = document.getElementById('para-count-input').value;
-    totalCountP = document.getElementById('red-count-input').value;
-    console.log(parasiteCount);
-    counterSave();
-    percentageCalculator();
-}
-
 
 
 
@@ -67,7 +52,6 @@ function updateCounter() {
 
 function percentageCalculator() {
     percentageP = ( parasiteCount / totalCountP ) * 100;
-    localStorage.setItem('percentageP', percentageP);
     document.getElementById("percent").textContent = percentageP.toFixed(2);
     return;
 }
@@ -78,29 +62,30 @@ function buttonChange() {
     } else if (buttonStateP = "Subtract") {
         buttonStateP = "Add";
     }
-    localStorage.setItem('buttonStateP', buttonStateP);
     document.getElementById("switch").textContent = buttonStateP;
 }
 
 function keyStroke(event) {
-    if (event.key == "ArrowLeft" && buttonStateP == "Add" && totalCountP < 1000) {
-        console.log("event.code is" + event.code + ". Parasite count before is: " + parasiteCount);
+    if (event.key == '1' && buttonStateP == "Add" && totalCountP < 1000) {
+        console.log("event.code is" + event.key + ". Parasite count before is: " + parasiteCount);
         parasiteCount++;
         totalCountP++;
         console.log("event.code is" + event.code + ". Parasite count after is: " + parasiteCount);
-    } else if (event.key == "ArrowLeft" && buttonStateP == "Subtract" && parasiteCount > 0) {
+    } else if (event.key == "1" && buttonStateP == "Subtract" && parasiteCount > 0) {
         parasiteCount--;
         if (totalCountP > 0) {
             totalCountP--;
         }
-    } else if (event.key === "ArrowRight" && buttonStateP == "Add" && totalCountP < 1000) {
+    } else if (event.key === "3" && buttonStateP == "Add" && totalCountP < 1000) {
         totalCountP++
-    } else if(event.key === "ArrowRight" && buttonStateP == "Subtract" && totalCountP > 0) {
+    } else if(event.key === "3" && buttonStateP == "Subtract" && totalCountP > 0) {
         totalCountP--
     }
     counterSave()
     percentageCalculator();
 }
+
+
 
 function buttonPress(inputId) {
     switch (inputId) {
@@ -130,9 +115,7 @@ function buttonPress(inputId) {
 
 //This function saves the counter and updates webpage
 function counterSave() {
-    localStorage.setItem('totalCountP', totalCountP);
     document.getElementById("total").textContent = totalCountP;
-    localStorage.setItem('parasiteCount', parasiteCount);
     document.getElementById("parasite").textContent = parasiteCount;
 }
 
